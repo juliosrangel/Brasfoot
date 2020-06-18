76238d94975c89d74939ac58ec7f6ce8a62abaf2
juliosrangel <julio.cesar.s.rangel@gmail.com>
2020-06-18 20:18:52 -0300

    Add files via upload

---
+++ Brasfut/brasfut/src/test/java/br/gov/sp/fatec/brasfut/BrasfutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.brasfut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasfutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
